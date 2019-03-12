d306513d18f53661b88dbfa714ce6b3153acf199
Matt Keyworth <45359792+LBHMKeyworth@users.noreply.github.com>
2019-03-12 09:29:37 +0000

    Added then commented out test

--- LBHVerificationHubAPITest/Test/UseCases/V1/VerifyUseCaseTest.cs
+++ LBHVerificationHubAPITest/Test/UseCases/V1/VerifyUseCaseTest.cs
@@ -12,14 +12,38 @@ using System.Threading;
 using LBHVerificationHubAPI.Infrastructure.V1.Exceptions;
 using LBHVerificationHubAPI.UseCases.V1.Objects;
 
-namespace LBHVerificationHubAPITest
+namespace LBHVerificationHubAPITest.Test.UseCases.V1
 {
     public class VerifyUseCaseTest
     {
         private readonly IVerifyUseCase _classUnderTest;
         private readonly Mock<IGateway> _fakeGateway;
 
+        public VerifyUseCaseTest()
+        {
+            _fakeGateway = new Mock<IGateway>();
 
-    }
-       
+            _classUnderTest = new VerifyUseCase(_fakeGateway.Object);
+        }
+
+        //[Fact]
+        //public async Task GivenValidedInput__WhenExecuteAsync_GatewayReceivesCorrectInput()
+        //{
+        //    //arrange
+        //    var tenancyAgreementRef = "Test";
+        //    _fakeGateway.Setup(s => s.veri(It.Is<ParkingPermitVerificationRequest>(i => i.TenancyRef.Equals("Test")), CancellationToken.None))
+        //        .ReturnsAsync(new PagedResults<TenancyListItem>());
+
+        //    var request = new ParkingPermitVerificationRequest
+        //    {
+                
+        //    };
+        //    //act
+        //    await _classUnderTest.ExecuteAsync(request, CancellationToken.None);
+        //    //assert
+        //    _fakeGateway.Verify(v => v.SearchTenanciesAsync(It.Is<ParkingPermitVerificationRequest>(i => i.TenancyRef.Equals("Test")), CancellationToken.None));
+        //}
+
+
+    }       
 }
