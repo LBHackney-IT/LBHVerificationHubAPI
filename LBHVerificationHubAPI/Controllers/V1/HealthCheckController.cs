c073024f1f9997867e240ae02af759cd5cca7225
Selwyn Preston <selwyn.preston@hackney.gov.uk>
2019-10-16 15:13:48 +0100

    Updated controller to implement the health check ping and error endpoints.

--- LBHVerificationHubAPI/Controllers/V1/HealthCheckController.cs
+++ LBHVerificationHubAPI/Controllers/V1/HealthCheckController.cs
@@ -1,17 +1,31 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using LBHVerificationHubAPI.UseCases.V1;
 
 namespace LBHVerificationHubAPI.Controllers.V1
 {
-    public class HealthCheckController
+    [ApiVersion("1")]
+    [Route("api/v1/healthcheck")]
+    [ApiController]
+    [Produces("application/json")]
+    public class HealthCheckController : BaseController
     {
-        public OkObjectResult HealthCheck()
+        [HttpGet]
+        [Route("ping")]
+        [ProducesResponseType(typeof(Dictionary<string, bool>), 200)]
+        public IActionResult HealthCheck()
         {
-            throw new System.NotImplementedException();
+            var result = new Dictionary<string, bool> {{"success", true}};
+
+            return Ok(result);
         }
 
+        [HttpGet]
+        [Route("error")]
         public void ThrowError()
         {
-            throw new System.NotImplementedException();
+            ThrowOpsErrorUseCase.Execute();
         }
+
     }
 }
