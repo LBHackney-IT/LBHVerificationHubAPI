c566113a8b6ad8628c49eeb52071f375d03b05ed
Selwyn Preston <selwyn.preston@hackney.gov.uk>
2019-10-16 12:21:58 +0100

    Added health check controller.

---
+++ LBHVerificationHubAPI/Controllers/V1/HealthCheckController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LBHVerificationHubAPI.Controllers.V1
+{
+    public class HealthCheckController
+    {
+        public OkObjectResult HealthCheck()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ThrowError()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
