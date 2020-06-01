56db2513b8d98f9fdc7a119aeb549f4be1901c38
Alexander Demetriou <alex.demetriou@hackney.gov.uk>
2020-06-01 15:04:15 +0100

    Append environment to table name

--- LBHVerificationHubAPI/Gateways/V1/VerdictDbGateway.cs
+++ LBHVerificationHubAPI/Gateways/V1/VerdictDbGateway.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Amazon.DynamoDBv2.DataModel;
 using LBHVerificationHubAPI.Domain;
 using LBHVerificationHubAPI.Infrastructure.V1.Context;
 
@@ -9,13 +10,19 @@ namespace LBHVerificationHubAPI.Gateways.V1
     {
         private readonly IVerdictDbContext _verdictDbContext;
 
-        public VerdictDbGateway(IVerdictDbContext verdictDbContext)
-            => _verdictDbContext = verdictDbContext;
+        private readonly DynamoDBOperationConfig _dbConfig = new DynamoDBOperationConfig
+        {
+            OverrideTableName =
+                $"verificationhub-logging-{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT").ToLower()}"
+        };
 
-        public async Task AppendVerdict(Verdict verdict) => 
-            await _verdictDbContext.SaveAsync(verdict);
+        public VerdictDbGateway(IVerdictDbContext verdictDbContext) =>
+            _verdictDbContext = verdictDbContext;
 
-        public async Task<Verdict> FetchVerdict(Guid verdictGuid) => 
-            await _verdictDbContext.LoadAsync<Verdict>(verdictGuid);
+        public async Task AppendVerdict(Verdict verdict) =>
+            await _verdictDbContext.SaveAsync(verdict, _dbConfig);
+
+        public async Task<Verdict> FetchVerdict(Guid verdictGuid) =>
+            await _verdictDbContext.LoadAsync<Verdict>(verdictGuid, _dbConfig);
     }
 }
