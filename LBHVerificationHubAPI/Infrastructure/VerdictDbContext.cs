6554b6a15409175e58f1f6f7773a22431f17cf1a
Alexander Demetriou <alex.demetriou@hackney.gov.uk>
2020-06-01 13:33:02 +0100

    Remove env var AWS auth

--- LBHVerificationHubAPI/Infrastructure/VerdictDbContext.cs
+++ LBHVerificationHubAPI/Infrastructure/VerdictDbContext.cs
@@ -9,24 +9,8 @@ namespace LBHVerificationHubAPI.Infrastructure.V1.Context
 {
     public class VerdictDbContext : DynamoDBContext, IVerdictDbContext
     {
-        public VerdictDbContext() : base(GetClient())
+        public VerdictDbContext() : base(new AmazonDynamoDBClient(RegionEndpoint.GetBySystemName("eu-west-2")))
         {
-            // TODO: Do some checking to make sure DB is OK and if not, set it up?.
-        }
-
-        private static AmazonDynamoDBClient GetClient()
-        {
-            var accessKey = Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID")
-                            ?? throw new Exception("No AWS_ACCESS_KEY_ID found in Environment.");
-            var secretKey = Environment.GetEnvironmentVariable("AWS_SECRET_ACCESS_KEY")
-                            ?? throw new Exception("No AWS_SECRET_ACCESS_KEY found in Environment.");
-            var regionName = Environment.GetEnvironmentVariable("AWS_REGION")
-                            ?? throw new Exception("No AWS_REGION found in Environment.");
-            
-            var credentials = new BasicAWSCredentials(accessKey, secretKey);
-            var region = RegionEndpoint.GetBySystemName(regionName);
-            
-            return new AmazonDynamoDBClient(credentials, region);
         }
     }
 }
