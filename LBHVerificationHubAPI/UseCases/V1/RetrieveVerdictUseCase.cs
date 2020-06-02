eb3f9e58fb9a7c335011aecd1c1ad0bd2e5d2d9d
Alexander Demetriou <alex.demetriou@hackney.gov.uk>
2020-06-02 15:47:20 +0100

    Add some logging for troubleshooting

--- LBHVerificationHubAPI/UseCases/V1/RetrieveVerdictUseCase.cs
+++ LBHVerificationHubAPI/UseCases/V1/RetrieveVerdictUseCase.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using LBHVerificationHubAPI.Boundary.V1;
 using LBHVerificationHubAPI.Gateways.V1;
 
@@ -15,9 +16,14 @@ namespace LBHVerificationHubAPI.UseCases.V1
 
         public async Task<VerdictResponse> ExecuteAsync(VerdictRequest request)
         {
+            Console.WriteLine(request.Guid);
+
+            Console.WriteLine($"Sending {request.Guid} to Use Case");
+            var verdict = await _verdictDbGateway.FetchVerdict(request.Guid);
+            
             return new VerdictResponse
             {
-                Verdict = await _verdictDbGateway.FetchVerdict(request.Guid)
+                Verdict = verdict
             };
         }
     }
